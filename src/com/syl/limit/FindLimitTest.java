aa6c0b7fcf258b3d7e9ba7d7955a395284dbfb9a
SYL <1061300519@qq.com>
2017-09-09 00:21:09 +0800

    加入 .gitignore文件，再次提交

--- src/com/syl/limit/FindLimitTest.java
+++ src/com/syl/limit/FindLimitTest.java
@@ -17,7 +17,7 @@ public class FindLimitTest {
     @Test
     public void printRes () throws Exception {
         int [] a = {13,5,34,11,12,50,18};
-        FindLimit fl = new FindLimit(a,"aaaaaaaaaaaa");
+        FindLimit fl = new FindLimit(a,"max");
         int[] res = fl.findBySign();
         // System.out.println(res[0]+"--"+res[1]+","+res[2]+"--"+res[3]);
         fl.printRes();
