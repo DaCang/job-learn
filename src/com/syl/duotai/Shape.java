b98ae066dbdfdea8b5a20ebdf841b673d0f37bd1
SYL <1061300519@qq.com>
2017-09-13 20:40:10 +0800

    多态的一些测试程序，接下来实现工厂模式以及动态代理

---
+++ src/com/syl/duotai/Shape.java
@@ -0,0 +1,9 @@
+package com.syl.duotai;
+
+public  abstract   class Shape {
+    String shapeName;
+
+    public void draw(){
+        System.out.println("我是父类，我也不知道画什么？？？？");
+    }
+}
---
+++ src/com/syl/duotai/Square.java
@@ -0,0 +1,9 @@
+package com.syl.duotai;
+
+public class Square extends  Shape {
+    @Override
+    public void draw () {
+        super.draw();
+        System.out.println("我是子类,我知道画什么,我画的是矩形(Square)");
+    }
+}
