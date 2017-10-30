b79253ecf78602a2cc6cb1f5d6902d2910f1ac48
SYL <1061300519@qq.com>
2017-10-30 15:11:31 +0800

    加入枚举

---
+++ src/com/syl/enumTest/OPerTest.java
@@ -0,0 +1,8 @@
+package com.syl.enumTest;
+
+public class OPerTest {
+    public static void main (String[] args) {
+        Oper oper = Oper.D;
+        System.out.println(oper.getOper());
+    }
+}
