b9730dff5a6f1a4b59e10e4b630cb3c3483a4def
SYL <1061300519@qq.com>
2018-02-21 16:18:01 +0800

    test valueOf method

--- src/main/java/com/syl/test/DemoTest.java
+++ src/main/java/com/syl/test/DemoTest.java
@@ -2,11 +2,33 @@ package com.syl.test;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class DemoTest {
+
+     /**
+       * @Title: testValueOfMethod
+       * @Description: （test valueOf method）
+       * @author 宋永利
+       * @date 2018/2/19
+       * @throws
+      */
     @Test
     public void testValueOfMethod(){
         Object object =null;
         String str1 = String.valueOf(object);
-        System.out.println(str1);
+        //这种打印
+        //System.out.println(str1);
+
+        try {
+            //或者这种
+            PrintStream printStream = new PrintStream("a.html");
+            printStream.println(object);
+        } catch (FileNotFoundException e) {
+            System.out.println("a");
+            e.printStackTrace();
+        }
+
     }
 }
