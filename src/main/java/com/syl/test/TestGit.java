0527bba975fb3e085be2aefd99edfee7ae53a6f5
SYL <1061300519@qq.com>
2018-02-23 17:10:05 +0800

    test

--- src/main/java/com/syl/test/TestGit.java
+++ src/main/java/com/syl/test/TestGit.java
@@ -3,17 +3,5 @@ package com.syl.test;
 public class TestGit {
     public static void main (String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
     }
 }
