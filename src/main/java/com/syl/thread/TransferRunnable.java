7c8ba483c4ffa542ea01205354cad201750213f3
SYL <1061300519@qq.com>
2018-02-23 17:29:57 +0800

    e

--- src/main/java/com/syl/thread/TransferRunnable.java
+++ src/main/java/com/syl/thread/TransferRunnable.java
@@ -4,7 +4,6 @@ package com.syl.thread;
  * P 20
  */
 public class TransferRunnable implements   Runnable {
-    @Override
     public void run () {
         Bank bank = new Bank();
         int toAccount = (int) (bank.size()*Math.random());
